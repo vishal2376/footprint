70c0ed87d490974fcb7b8e875be4596598890dcb
Vishal Singh <vishalsingh2376@gmail.com>
2025-12-24 21:18:41 +0530

    init

---
+++ composeApp/src/commonMain/kotlin/com/nullappstudios/footprint/Greeting.kt
@@ -0,0 +1,9 @@
+package com.nullappstudios.footprint
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/nullappstudios/footprint/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.nullappstudios.footprint
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
